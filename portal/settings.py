496f12082a717746e90f6746499de75663535cb2
Alejandro Romero <alejo8591@gmail.com>
2012-07-04 22:08:29 -0500

    update settings for heroku

--- portal/settings.py
+++ portal/settings.py
@@ -15,8 +15,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
+        'NAME': 'portal',                      # Or path to database file if using sqlite3.
+        'USER': 'portal',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
