d6edf732a0726a0e57de2e455a47c165c1026e32
Alejandro Romero <alejo8591@gmail.com>
2012-07-11 16:13:41 -0500

    push admin in blogman app

---
+++ blogman/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.flatpages.models import FlatPage
+from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
+from blogman.models import Category, Entry
+
+class FlatPageAdmin(FlatPageAdminOld):
+    class Media:
+        js = ('/Users/alejo8591/Documents/portal/Portal/portal/static/tiny_mce/tiny_mce.js',
+              '/Users/alejo8591/Documents/portal/Portal/portal/static/tiny_mce/textareas.js',)
+        
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':['b_title']}
+    
+class EntryAdmin(admin.ModelAdmin):
+    prepopulated_fields = { 'slug': ['e_title'] }
+        
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Entry, EntryAdmin)
