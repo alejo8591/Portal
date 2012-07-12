c3a99979977ec91cda20d2dbf7aad1aed8e8a31e
Alejandro Romero <alejo8591@gmail.com>
2012-07-12 00:51:05 -0500

    update urls with blogman

--- portal/urls.py
+++ portal/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from blogman.views import entries_index 
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -13,8 +14,11 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^blogman/', 'blogman.views.entries_index'),
+    url(r'^blogman/(?P<year>\d{4})/(?P<month>\w{4})/(?P<day>\d{2})/(?P<e_slug>[-\w]+)/$',
+        'blogman.views.entry_detail'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('django.contrib.flatpages.urls')),
+    url(r'^', include('django.contrib.flatpages.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
 )
