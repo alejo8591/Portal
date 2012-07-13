7be75636a87159c2e3cfd41e5e713f6ca33977f0
Alejandro Romero <alejo8591@gmail.com>
2012-07-13 01:45:38 -0500

    update urls with apps

--- portal/urls.py
+++ portal/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import patterns, include, url
-from blogman.views import entries_index 
+from blogman.views import entries_index
+from blogman.models import Entry
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+from django.views.generic.date_based import object_detail
+entry_info_dict = {
+    'queryset': Entry.objects.all(),
+    'date_field' : 'pub_date',
+}
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,13 +21,16 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^blogman/', 'blogman.views.entries_index'),
-    url(r'^blogman/(?P<year>\d{4})/(?P<month>\w{4})/(?P<day>\d{2})/(?P<e_slug>[-\w]+)/$',
-        'blogman.views.entry_detail'),
+    url(r'^blogman/$', 'django.views.generic.date_based.archive_index', entry_info_dict),
+    url(r'^blogman/(?P<year>\d{4})/$','django.views.generic.date_based.archive_year', entry_info_dict),
+    url(r'^blogman/(?P<year>\d{4})/(?P<month>\w{3})/$', 'django.views.generic.date_based.archive_month', entry_info_dict),
+    url(r'^blogman/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'django.views.generic.date_based.archive_day', entry_info_dict),
+    url(r'^blogman/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+        'object_detail', entry_info_dict),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('django.contrib.flatpages.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
+    #url(r'^', include('django.contrib.flatpages.urls')),
 )
 
 from django.conf import settings
