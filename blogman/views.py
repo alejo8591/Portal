2057c16d344e2c26aa9f33a57e01baae6e97f4a1
Alejandro Romero <alejo8591@gmail.com>
2012-07-13 01:41:32 -0500

    update views with news changes

--- blogman/views.py
+++ blogman/views.py
@@ -4,14 +4,3 @@ from blogman.models import Entry
 def entries_index(request):
     return render_to_response('blogman/entry_index.html',
                               {'entry_list': Entry.objects.all()})
-    
-def entry_detail(request, year, month, day, slug):
-    import datetime, time
-    date_stamp = time.strptime(year+month+day, "%Y%b%d")
-    pub_date = datetime.date(*date_stamp[:3])
-    entry = get_object_or_404(Entry, e_pub_date__year=pub_date.year,
-                                e_pub_date__month=pub_date.month,
-                                e_pub_date__day=pub_date.day,
-                                e_slug=slug)
-    return render_to_response('blogman/entry_detail.html',
-                              {'entry': entry })
