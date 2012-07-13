a9c2808f241e4022f590d14e23b2e381e9b0deeb
Alejandro Romero <alejo8591@gmail.com>
2012-07-13 01:42:45 -0500

    update settings with new values

--- portal/settings.py
+++ portal/settings.py
@@ -19,8 +19,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'portal',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '3193115',                  # Not used with sqlite3.
+        'USER': 'toromobile',                      # Not used with sqlite3.
+        'PASSWORD': 'toromobile',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
