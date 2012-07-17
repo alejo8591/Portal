ebeccc3cf2df9bd13f500cb31cbbe3765fefdf6d
Alejandro Romero <alejo8591@gmail.com>
2012-07-16 23:39:07 -0500

    decoupling URLs

---
+++ blogman/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, url, include
+#from django.conf.urls.defaults import *
+from blogman.models import Entry
+
+entry_info_dict = {
+    'queryset': Entry.objects.all(),
+    'date_field' : 'pub_date',
+}
+
+urlpatterns = patterns('django.views.generic.date_based',
+    (r'^$','archive_index', entry_info_dict,
+     'blogman_entry_archive_index'),
+    (r'^(?P<year>\d{4})/$', 'archive_year',  entry_info_dict,
+     'blogman_entry_archive_year'),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/$','archive_month',  entry_info_dict,
+     'blogman_entry_archive_month'),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day',  entry_info_dict,
+     'blogman_entry_archive_month'),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', entry_info_dict,'blogman_entry_archive_month', 'blogman_entry_detail'),
+)
