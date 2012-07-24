812a0a15e4be6f7ed7846afeb819f2198c51715c
Alejandro Romero <alejo8591@gmail.com>
2012-07-24 11:51:52 -0500

    update views from blogman app

--- blogman/views.py
+++ blogman/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import get_object_or_404, render_to_response
 from blogman.models import Entry
 
 def entries_index(request):
-    return render_to_response('blogman/entry_index.html',
-                              {'entry_list': Entry.objects.all()})
+    return render_to_response('blogman/entryndex.html',
+                              {'entry_list': Entry.objects.filter(status=Entry.LIVE_STATUS)})
+
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    return render_to_response('blogman/category_detail.html',
+                              {'object_list':category.live_entry_set.all()})
