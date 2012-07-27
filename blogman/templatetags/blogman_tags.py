02e45a09e67b0587825d60ff4830630fdd4c4520
Alejandro Romero <alejo8591@gmail.com>
2012-07-27 17:18:28 -0500

    update for new funtions with new tags

--- blogman/templatetags/blogman_tags.py
+++ blogman/templatetags/blogman_tags.py
@@ -1,31 +1,15 @@
-from django.db.models import get_model
+#from django.db.models import get_model
 from django import template
+from blogman.models import Entry
 
-def do_latest_content(parser, token):
-    bits = token.split_contents()
-    if len(bits) != 5:
-        raise template.TemplateSyntaxError("'get_latest_content' tag takes exactly four arguments")
-    model_args = bits[1].split('.')
-    if len(model_args) != 2:
-        raise template.TemplateSyntaxError("First argument to 'get_latest_content' must be an 'application name'.'model name' string")
-    model = get_model(*model_args)
-    if model is None:
-        raise template.TemplateSyntaxError("'get_latest_content' tag got an invalid model: %s" % bits[1])
-    return LatestContentNode(model, bits[2], bits[4])
-
-class LatestContentNode(template.Node):
-    def __init__(self, model, num, varname):
-        self.model = model
-        self.num = int(num)
-        self.varname = varname
-        
+class LatestEntriesNode(template.Node):
     def render(self, context):
-        manager = self.model._default_manager
-        if self.model.__name__ == 'Entry':
-            manager = self.model.live
-        context[self.varname] = manager.all()[:self.num]
+        context['latest_entries'] = Entry.live.all()[:5]
         return ''
 
-register = template.Library()
-register.tag('get_latest_content', do_latest_content)
 
+def do_latest_entries(parser, token):
+    return LatestEntriesNode()
+
+register = template.Library()
+register.tag('get_latest_entries', do_latest_entries)
