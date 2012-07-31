e5f00183dccd973e1d74efee63f99b2f94976a5c
Alejandro Romero <alejo8591@gmail.com>
2012-07-31 15:53:41 -0500

    update template tags

--- blogman/templatetags/blogman_tags.py
+++ blogman/templatetags/blogman_tags.py
@@ -7,9 +7,11 @@ class LatestEntriesNode(template.Node):
         context['latest_entries'] = Entry.live.all()[:5]
         return ''
 
-
 def do_latest_entries(parser, token):
-    return LatestEntriesNode()
+    bits = token.split_contents()
+    if len(bits) != 5:
+        raise template.TemplateSyntaxError("'get_latest_content' tag takes exactly for arguments")
+    return LatestContentNode(bits[1], bits[2], bits[4])
 
 register = template.Library()
 register.tag('get_latest_entries', do_latest_entries)
