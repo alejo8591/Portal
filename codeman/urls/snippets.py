9d9e8a209d2de34816575d64c508ea359345c95d
Alejandro Romero <alejo8591@gmail.com>
2012-08-21 14:54:43 -0500

    update urls of snippets

--- codeman/urls/snippets.py
+++ codeman/urls/snippets.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url, include
+from django.conf.urls.defaults import *
+#from django.conf.urls import patterns, url, include
 from django.views.generic.list_detail import object_detail, object_list
 from codeman.models import Snippet
 
 snippet_query = {'queryset':Snippet.objects.all()}
 urlpatterns = patterns('',
-    url(r'^$', object_list, dict(snippet_query, paginate_by=10)),
-    url(r'^(P<object_id>\d+)/$', object_detail, snippet_query,
-        name='codeman_snippet_detail'),
+    url(r'^$', object_list, dict(snippet_query, paginate_by=10), name='codeman_snippet_list'),
+    url(r'^(P<object_id>\d+)/$', object_detail, snippet_query, 'codeman_snippet_detail'),
 )
