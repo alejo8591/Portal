8394aef1f3d330a091d53f3d3378c8eeb725e689
Alejandro Romero <alejo8591@gmail.com>
2012-08-21 12:46:37 -0500

    update urls for codeman app

--- codeman/urls/languages.py
+++ codeman/urls/languages.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url, include
 from django.views.generic.list_detail import object_list
 from codeman.models import Language
+from codeman.views.languages import language_detail
 
 language_info = {
     'queryset': Language.objects.all(),
@@ -8,5 +9,5 @@ language_info = {
 }
 urlpatterns = patterns('',
                        url(r'^', object_list, language_info, name='codeman_language_list'),
-                       #url(r'^(?P<slug>[-\w]+)/$', language_detail, name='codeman_language_detail'),
+                       url(r'^(?P<slug>[-\w]+)/$', language_detail, name='codeman_language_detail'),
 )
