21be486b1052b2a52a60e501e1ad529fe0dc9d70
Alejandro Romero <alejo8591@gmail.com>
2012-08-21 14:53:04 -0500

    update models for codeman app

--- codeman/models.py
+++ codeman/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 from pygments import formatters, highlight, lexers
 from taggit.managers import TaggableManager
 from datetime import datetime, date
-from markdown import markdown 
+from markdown import markdown
+
 
 class Language(models.Model):
     name = models.CharField(max_length=120)
@@ -43,6 +44,13 @@ class Snippet(models.Model):
     def __unicode__(self):
         return self.title
     
+    def save(self, force_insert=False, force_update=False):
+        if not self.id: self.pub_date = datetime.now()
+        self.update_date = datetime.now()
+        self.description_html = markdown(self.description)
+        self.highlighted_code = self.highlight()
+        super(Snippet, self).save(force_insert, force_update)
+        
     def highlight(self):
         """
             linenos=True argument 
@@ -52,10 +60,6 @@ class Snippet(models.Model):
         """
         return highlight(self.code, self.language.get_lexer(),
                          formatters.HtmlFormatter(linenos=True))
-    
-    def save(self, force_insert=False, force_update=False):
-        if not self.id: self.pub_date = datetime.now()
-        self.update_date = datetime.now()
-        self.description_html = markdown(self.description)
-        self.highlighted_code = self.highlight()
-        super(Snippet, self).save(force_insert, force_update)          
+    @models.permalink
+    def get_absolute_url(self):
+        return ('codeman_snippet_detail', (), {'object_id': self.id})
