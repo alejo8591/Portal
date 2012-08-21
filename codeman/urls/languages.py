60735d5ed0608b4a3e0ab06f5ee16b04536efc0c
Alejandro Romero <alejo8591@gmail.com>
2012-08-21 12:24:45 -0500

    push urls for codeman app

---
+++ codeman/urls/languages.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url, include
+from django.views.generic.list_detail import object_list
+from codeman.models import Language
+
+language_info = {
+    'queryset': Language.objects.all(),
+    'paginate_by': 10
+}
+urlpatterns = patterns('',
+                       url(r'^', object_list, language_info, name='codeman_language_list'),
+                       #url(r'^(?P<slug>[-\w]+)/$', language_detail, name='codeman_language_detail'),
+)
