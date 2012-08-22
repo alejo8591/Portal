8047da0ab7786446492a9fe14da377f655b9a9b4
Alejandro Romero <alejo8591@gmail.com>
2012-08-22 17:46:01 -0500

    update settings

--- portal/settings.py
+++ portal/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'portal',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '3193115',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -153,4 +153,4 @@ LOGGING = {
     }
 }
 #Akismet api key
-AKISMET_API_KEY = '6683e7b5e67b'
+AKISMET_API_KEY = ''
