f6ed8e2c5dc3557c92148084dae7b62188d8c12c
Alejandro Romero <alejo8591@gmail.com>
2012-10-03 17:32:16 -0500

    updates urls

--- portal/urls.py
+++ portal/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from dajaxice.core import dajaxice_config, dajaxice_autodiscover
 from blogman.views import entries_index
 from django.conf import settings
 from blogman.feeds import LatestEntriesFeed
 from codeman.views import languages
+
+dajaxice_autodiscover()
 
 feeds = { 'entries': LatestEntriesFeed }
 
@@ -14,6 +18,7 @@ from django.views.generic.date_based import object_detail
 urlpatterns = patterns('',
     (r'^grappelli/', include('grappelli.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     (r'^blogman/', include('blogman.urls.entries')),
     (r'^blogman/categories/', include('blogman.urls.categories')),
     (r'^blogman/links/', include('blogman.urls.links')),
@@ -23,6 +28,8 @@ urlpatterns = patterns('',
     (r'^snippets/', include('codeman.urls.snippets')),
     (r'^', include('django.contrib.flatpages.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
